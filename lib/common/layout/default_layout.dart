a730915fa4aa49e198cb39f31c4a7a655a29ee83
JooSeunghyeon <tmdgus4475@gamil.com>
2023-09-03 23:23:09 +0900

    Dio 작업 및 로그인 시스템 SplashScreen 구현

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
 class DefaultLayout extends StatelessWidget {
+  final Color? backgroundColor;
   final Widget child; // 위젯을 상속받는다.
 
-  const DefaultLayout({required this.child, super.key});
+  const DefaultLayout({required this.child, super.key, this.backgroundColor});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white, // 기본색상은 흰색으로 시작한다.
+      backgroundColor: backgroundColor ?? Colors.white, // 기본색상은 흰색으로 시작한다.
       body: child,
     );
   }
--- lib/user/view/login_screen.dart
+++ lib/user/view/login_screen.dart
@@ -1,14 +1,39 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:actual/common/const/colors.dart';
+import 'package:actual/common/const/data.dart';
 import 'package:actual/common/layout/default_layout.dart';
+import 'package:actual/common/view/root_tab.dart';
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import '../../common/component/custom_text_form_field.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String username = '';
+  String password = '';
+
+  @override
   Widget build(BuildContext context) {
+
+
+    final dio = Dio();
+
+    // localhost
+    final emulatorIp = '172.30.1.70:3000'; // 디바이스 연결해서 사용하는경우 같은 와이파이 영역대 잡아두기.
+    final simulatorIp = '127.0.0.1:3000';
+
+    final ip = Platform.isIOS ? simulatorIp : emulatorIp;
+
     return DefaultLayout( // 위젯에 중복 방지를 위해 디폴드레이아웃을 만든다.
       child: SingleChildScrollView( // 키보드 오버플로우 현상방지 스크롤로 만듬.
         keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag, // 드래그를 했을때 키보가 사라진다
@@ -31,28 +56,72 @@ class LoginScreen extends StatelessWidget {
                 ),
                 CustomTextFormField(
                   hintText: '이메일을 입력해주세요.',
-                  onChanged: (String value) {},
+                  onChanged: (String value) {
+                    username = value;
+                  },
                 ),
                 const SizedBox(
                   height: 16.0,
                 ),
                 CustomTextFormField(
                   hintText: '비밀번호를 입력해주세요.',
-                  onChanged: (String value) {},
+                  onChanged: (String value) {
+                    password = value;
+                  },
                   obscureText: true,
                 ),
                 const SizedBox(
                   height: 16.0,
                 ),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () async{
+                    //  ID:비밀번호
+                    // 사용자 한테 : 기준으로 아이디 비밀번호를 받고 Base64로 인코딩하는작업.
+                    final rawString = '$username:$password';
+
+                    Codec<String, String> stringToBase64 = utf8.fuse(base64); //이건 외우는게 좋음 .
+
+                    String token = stringToBase64.encode(rawString);
+
+                    final resp = await dio.post('http://$ip/auth/login',
+                      options: Options(
+                        headers: {
+                          'authorization': 'Basic $token',
+                        },
+                      ),
+                    );
+
+                    final refreshToken = resp.data['refreshToken'];
+                    final accessToken = resp.data['accessToken'];
+
+                    await storge.write(key: REFRESH_TOKEN_KEY, value: refreshToken);
+                    await storge.write(key: ACCESS_TOKEN_KEY, value: accessToken);
+
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (_) => RootTab(),
+                      ),
+                    );
+                  },
                   style: ElevatedButton.styleFrom(
                     primary: PRIMARY_COLOR,
                   ),
                   child: Text('로그인'),
                 ),
                 TextButton(
-                  onPressed: () {},
+                  onPressed: () async{
+                    final refreshToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAY29kZWZhY3RvcnkuYWkiLCJzdWIiOiJmNTViMzJkMi00ZDY4LTRjMWUtYTNjYS1kYTlkN2QwZDkyZTUiLCJ0eXBlIjoicmVmcmVzaCIsImlhdCI6MTY5Mzc0MDc1OCwiZXhwIjoxNjkzODI3MTU4fQ.W2WXMJEnJzx-ZRg-o85kI_i0MinUJRhVTvLMMvSyQoc';
+
+                    final resp = await dio.post('http://$ip/auth/token',
+                      options: Options(
+                        headers: {
+                          'authorization': 'Bearer $refreshToken',
+                        },
+                      ),
+                    );
+
+                    print(resp.data);
+                  },
                   style: TextButton.styleFrom(
                     primary: Colors.black,
                   ),
