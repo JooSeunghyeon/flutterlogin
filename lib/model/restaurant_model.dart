c4f933fe9bfd4ec5db9e22dad7df4c7e751e9262
JooSeunghyeon <tmdgus4475@gamil.com>
2023-09-24 17:38:10 +0900

    model 작업

---
+++ lib/model/restaurant_model.dart
@@ -0,0 +1,31 @@
+enum RestaurantPriceRange{
+  expensive,
+  medium,
+  cheap,
+}
+
+
+class RestarurantModel{
+  final String id;
+  final String name;
+  final String thumbUrl;
+  final List<String> tags;
+  final RestaurantPriceRange priceRange;
+  final double ratings;
+  final int ratingsCount;
+  final int deliveryTime;
+  final int deliveryFee;
+
+  RestarurantModel({
+    required this.id,
+    required this.name,
+    required this.thumbUrl,
+    required this.tags,
+    required this.priceRange,
+    required this.ratings,
+    required this.ratingsCount,
+    required this.deliveryTime,
+    required this.deliveryFee,
+});
+
+}
--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -1,4 +1,5 @@
 import 'package:actual/common/const/data.dart';
+import 'package:actual/model/restaurant_model.dart';
 import 'package:actual/restaurant/component/restaurant_card.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
@@ -6,7 +7,8 @@ import 'package:flutter/material.dart';
 class RestaurantScreen extends StatelessWidget {
   const RestaurantScreen({super.key});
 
-  Future<List> paginateRestaurant() async { // async 로 반환을 할려면 Future를 써야한다.
+  Future<List> paginateRestaurant() async {
+    // async 로 반환을 할려면 Future를 써야한다.
     final dio = Dio();
 
     final accessToken = await storge.read(key: ACCESS_TOKEN_KEY);
@@ -37,25 +39,42 @@ class RestaurantScreen extends StatelessWidget {
                 return Container();
               }
 
-              return ListView.separated( // PostMan 기준으로 화면을 가져옴 따로 서버가 있다 TS + Node 기반 .
+              return ListView.separated(
+                // PostMan 기준으로 화면을 가져옴 따로 서버가 있다 TS + Node 기반 .
                 itemBuilder: (_, index) {
-                  final item = snapshot.data![index]; // 각각의 번호를 item에 넣는다 약간 ? for 문으로 index 지정해주는거랑 비슷 ?
+                  final item = snapshot.data![
+                      index]; // 각각의 번호를 item에 넣는다 약간 ? for 문으로 index 지정해주는거랑 비슷 ?
+                  // parsed 변환되었다는 뜻
+                  final pItem = RestarurantModel( // 클래스르 인스턴스화를 해야 자동완성 기능으로 쓸수가 있다.
+                    id: item['id'],
+                    name: item['name'],
+                    thumbUrl: 'http://$ip${item['thumbUrl']}',
+                    tags: List<String>.from(item['tags']),
+                    priceRange: RestaurantPriceRange.values.firstWhere( // enum 이라서 서버에서 가지고온 데이터 첫번째가 값이 같은걸 넣어준다는 뜻
+                      (e) => e.name == item['priceRange'],
+                    ),
+                    ratings: item['ratings'],
+                    ratingsCount: item['ratingsCount'],
+                    deliveryTime: item['deliveryTime'],
+                    deliveryFee: item['deliveryFee'],
+                  );
 
                   return RestaurantCard(
                     image: Image.network(
-                      'http://$ip${item['thumbUrl']}',
+                      pItem.thumbUrl,
                       fit: BoxFit.cover,
                     ),
                     // image: Image.asset(
                     //   'asset/img/food.jpg',
                     //   fit: BoxFit.cover,
                     // ),
-                    name: item['name'],
-                    tags: List<String>.from(item['tags']), // List<dynamic> 으로 들어와야하는데 List<String> 으로 들어와서 형변환 필요
-                    ratingsCount: item['ratingsCount'],
-                    deliveryTime: item['deliveryTime'],
-                    ratings: item['ratings'],
-                    deliveryFee: item['deliveryFee'],
+                    name: pItem.name,
+                    tags: pItem.tags,
+                    // List<dynamic> 으로 들어와야하는데 List<String> 으로 들어와서 형변환 필요
+                    ratingsCount: pItem.ratingsCount,
+                    deliveryTime: pItem.deliveryTime,
+                    ratings: pItem.ratings,
+                    deliveryFee:pItem.deliveryFee,
                   );
                 },
                 separatorBuilder: (_, index) {
